9ea9f6403f767d433ff795c36f72fff0d9de87d8
Vishal Jeet <vjeet10@gmail.com>
2013-09-08 17:39:04 -0400

    Initialize repository

---
+++ assignment2_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assignment2App::Application.config.secret_token = 'fd9406fbe5f2ce7a5717ed4abed1c38c99bcbb64ce5657fec284c7a44e4fd1b35b354583587c6cb6fcb3a0f5e5cfa9f8e3a31b6f61eecdf5e80bbd2be166a720'
